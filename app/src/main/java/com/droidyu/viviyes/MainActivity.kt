8cc7167c42d6332a84c1423f27238455aa93bfb4
droidYu <1370576461@qq.com>
2022-03-26 08:14:45 +0800

    init

---
+++ app/src/main/java/com/droidyu/viviyes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.droidyu.viviyes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
